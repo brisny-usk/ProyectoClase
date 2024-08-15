46f651961ae71847315a6f1c34bc51d9802b4ec2
brisny-usk <brisnyrivera468@gmail.com>
2024-08-15 13:33:47 -0600

    actualizacion de contion en clases program
    concatenación

--- ProyectoClase/Program.cs
+++ ProyectoClase/Program.cs
@@ -1,5 +1,27 @@
 ﻿
-double Numero1;
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hola Mundo");
-Numero1=Convert.ToDouble(Console.ReadLine());
+
+
+public class Program
+{
+    static void Main(string[]args)
+    {
+        int EdadMeses, Edad;
+        string Nombre;
+        Console.WriteLine("Hola vienvenidos a nuetro primer sistemacreado en C#");
+        Console.WriteLine("ingresa el nombre para contuinar");
+        Nombre = Console.ReadLine();// Para escribir valores de caracter
+        Console.WriteLine("Ingrese su edad ");
+        Edad = Convert.ToInt32(Console.ReadLine ());//combertir a datos dobule
+        EdadMeses = Edad * 12;//calculos matematicos 
+        Console.WriteLine(Nombre + "Tu edad en meses es:" + EdadMeses + "Meses");//concatenación
+    }
+
+
+
+
+
+
+}
+
+
+
